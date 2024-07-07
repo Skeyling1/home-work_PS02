31f4631fef532435a4d55006e3caaebd1b5c3dc8
Skeyling1 <skeyling1@gmail.com>
2024-07-07 10:29:29 +0300

    Update main3.py

---
+++ main3.py
@@ -0,0 +1,17 @@
+# Задание 3: Отправка данных
+# # 1. Используйте API, которое принимает POST-запросы для создания новых данных (например, https://jsonplaceholder.typicode.com/posts).
+# # 2. Создайте словарь с данными для отправки (например, `{'title': 'foo', 'body': 'bar', 'userId': 1}`).
+# # 3. Отправьте POST-запрос с этими данными.
+# # 4. Распечатайте статус-код и содержимое ответа.
+
+import requests
+import pprint
+
+parameter = {"userId" : 1}
+
+
+reslt = requests.get('https://jsonplaceholder.typicode.com/posts', params=parameter, )
+
+print(reslt.status_code)
+
+pprint.pprint(reslt.json())
