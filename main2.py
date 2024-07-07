31b21a4fa3c1e9df81d6eb6dddd39a7544ce22cf
Skeyling1 <skeyling1@gmail.com>
2024-07-07 10:22:57 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -2,3 +2,15 @@
 # # 1. Используйте API, который позволяет фильтрацию данных через URL-параметры (например, https://jsonplaceholder.typicode.com/posts).
 # # 2. Отправьте GET-запрос с параметром `userId`, равным `1`.
 # # 3. Распечатайте полученные записи.
+
+import requests
+import pprint
+
+parameter = {"userId" : 1}
+
+
+reslt = requests.get('https://jsonplaceholder.typicode.com/posts', params=parameter, )
+
+print(reslt.status_code)
+
+pprint.pprint(reslt.json())
